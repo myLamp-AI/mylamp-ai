b0284ff35df5ecbb8610a58f810401d87000d34c
Anubhav shaurya <97554087+anubhav220@users.noreply.github.com>
2024-05-29 11:25:27 +0530

    updated

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,13 +1,15 @@
 
 import MainCommunity from '@/components/community/mainCommunity'
+import MainNavbar from '@/components/MainNavbar';
 
 
 
 const page = () => {
   return (
-    <>
-        <MainCommunity />
-    </>
+    <div className=' flex relative w-full h-fit'>
+      <div className='h-96 w-2/12 hidden lg:block'><MainNavbar/></div>
+      <div className='fixed right-0 lg:w-10/12 w-full'> <MainCommunity/></div>
+    </div>
   )
 }
 
