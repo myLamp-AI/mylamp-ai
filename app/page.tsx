0199a67dfa5c3bee2e6fb089722f50543ede90d2
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2024-05-31 04:56:19 +0530

    Hero Section Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 "use client"
 
-import Sidebar from '@/components/Sidebar'
-import Practice from '@/components/Practice-1'
-import Explore from '@/components/Explore'
-import NewSidebar from '@/components/NewSidebar'
+// import Sidebar from '@/components/Sidebar'
+// import Practice from '@/components/Practice-1'
+// import Explore from '@/components/Explore'
+// import NewSidebar from '@/components/NewSidebar'
+import HeroSection from "@/components/home/HeroSection";
 
 
 
 const page = () => {
   return (
     <>
-      <div className=''>
-        {/*  */}
-        <NewSidebar/>
+      <div className="flex flex-col bg-blue-300 ">
+        <HeroSection />
       </div>
     </>
   )
