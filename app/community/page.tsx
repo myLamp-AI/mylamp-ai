6cd1b10946e98d4e4c004e55b169ed616b526dde
Anubhav shaurya <97554087+anubhav220@users.noreply.github.com>
2024-05-30 15:52:40 +0530

    updated

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,14 +1,15 @@
 
 import MainCommunity from '@/components/community/mainCommunity'
-import MainNavbar from '@/components/MainNavbar';
+import NewSidebar from '@/components/NewSidebar';
+
 
 
 
 const page = () => {
   return (
-    <div className=' flex relative w-full h-fit'>
-      <div className='h-96 w-2/12 hidden lg:block'><MainNavbar/></div>
-      <div className='fixed right-0 lg:w-10/12 w-full'> <MainCommunity/></div>
+    <div className=' flex w-full h-screen'>
+      <div className='h-96 w-2/12 hidden lg:block'><NewSidebar/></div>
+      <div className='fixed right-0 lg:w-4/5 w-full'> <MainCommunity/></div>
     </div>
   )
 }
--- components/community/machinelearning.tsx
+++ components/community/machinelearning.tsx
@@ -1,66 +1,58 @@
-
 import Image from 'next/image';
 
 const MachineLearning: React.FC = () => {
   return (
-    <>
-    <div className='  w-full h-full flex  flex-col rounded-lg relative '>
+    <div className="w-full h-full flex flex-col rounded-lg relative">
+      {/* Header Section */}
+      <div className="w-full bg-[#8C52FF] absolute top-0 h-16 md:h-20 rounded-lg flex items-center justify-between p-4">
+        <div className="bg-white w-14 h-14 rounded-full flex justify-center items-center">
+          <Image
+            src="/web developement.svg"
+            alt="Web Development Logo"
+            width={30}
+            height={30}
+          />
+        </div>
+        <div className="text-2xl text-white font-semibold flex justify-center">
+          <h1>Machine Learning</h1>
+        </div>
+        <div className="flex space-x-4">
+          <Image
+            src="/Group 15.svg"
+            alt="Group Logo"
+            width={30}
+            height={29}
+          />
+          <Image
+            src="/Clip path group-3.svg"
+            alt="Clip Path Group"
+            width={20}
+            height={20}
+          />
+        </div>
+      </div>
       
-    <div className='w-full bg-[#8C52FF] absolute top-0 h-1/10 rounded-lg flex items-center justify-between'>
-     <div className='translate-x-8 bg-[#FFFF] w-14 h-14 rounded-full flex justify-center items-center'>
-           <Image
-           src="/web developement.svg"
-           alt="Verceldcjncn Logo"
-           className=""
-           width={30}
-           height={30}
-            />
-      </div>   
-     <div className='translate-x-10 text-2xl lg:translate-x-10 text-white font-semibold flex justify-center md:translate-x-4'><h1>Machine Learning</h1></div>  
-     <div className=' lg:translate-x-10 md:translate-x-3 '>
-     <Image
-           src="/Group 15.svg"
-           alt="Verceldcjncn Logo"
-           className=""
-           width={30}
-           height={29}
-            />
-      </div>  
-     <div className='-translate-x-3 '>
-      
-     <Image
-           src="/Clip path group-3.svg"
-           alt="Verceldcjncn Logo"
-           className=""
-           width={20}
-           height={20}
-            />
-      </div>  
-    </div>
-      <div className='w-11/12 flex absolute justify-between transform -translate-x-1/2 -translate-y-1/2 h-10 md:h-10 xl:h-14 border bottom-0 left-1/2 border-[#D9D9D9] rounded-lg'>
-       <div className='ml-2 w-fit flex items-center justify-center'>
-           <Image
-           src="/Clip path group.svg"
-           alt="Verceldcjncn Logo"
-           className=""
-           width={35}
-           height={30}
-            />
-       </div>
-       <div className='mr-2 w-fit flex items-center justify-center'>
-           <Image
-           src="/Clip path group-2.svg"
-           alt="Verceldcjncn Logo"
-           className=""
-           width={30}
-           height={29}
-            />
-       </div>
-     
+      {/* Bottom Section */}
+      <div className="w-11/12 flex absolute bottom-4 left-1/2 transform -translate-x-1/2 h-10 md:h-14 border border-[#D9D9D9] rounded-lg">
+        <div className="ml-2 w-fit flex items-center justify-center">
+          <Image
+            src="/Clip path group.svg"
+            alt="Clip Path Group"
+            width={35}
+            height={30}
+          />
+        </div>
+        <div className="mr-2 w-fit flex items-center justify-center">
+          <Image
+            src="/Clip path group-2.svg"
+            alt="Clip Path Group 2"
+            width={30}
+            height={29}
+          />
+        </div>
       </div>
     </div>
-    </>
-  )
+  );
 }
 
 export default MachineLearning;
