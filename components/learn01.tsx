9ef5ecfcfd37dff53510d08ebe77cf365495fad2
Anubhav shaurya <97554087+anubhav220@users.noreply.github.com>
2024-06-02 12:23:39 +0530

    updated

--- components/learn01.tsx
+++ components/learn01.tsx
@@ -27,26 +27,62 @@ const Sidebar02 = () => {
                    </div>
 
 
-                  <div className=" md:w-[70%] md:h-full bg-white rounded-b lg:rounded-b-none relative lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                    <div className="mb-8 h-full">
+                  <div className=" md:w-[70%] h-full bg-white rounded-b lg:rounded-b-none  lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                    <div className="mb-8 relative h-full">
                       <p className="text-sm text-gray-600 flex items-center"></p>
-                      <div className="text-gray-900 font-bold text-xl mb-2 ">
+                      <div className="text-gray-900 font-semibold xl:text-3xl mt-2 sm:text-[19px] lg:text-2xl ">
                         Introduction to Machine Learning
                       </div>
-                      <div className="flex absolute md:bottom-20 sm:bottom-10 items-center justify-center bg-red-300">dvfvf
+                      <div className="flex absolute md:top-10 w-full lg:mt-3 md:mt-6  items-center text-[#0166C8] justify-start flex-row gap-4">
+                      <div className="w-full lg:w-32 sm:w-20 bg-[#E8E2F4] p-[1px] text-center rounded-sm md:mr-4 text-sm lg:text-xl">3 weeks</div>
+                       <div className="w-full lg:w-32 sm:w-20 bg-[#E8E2F4] p-[1px] rounded-sm text-center text-sm lg:text-xl">6 months</div>
+                        </div>
+
+                      <div className="flex absolute md:top-28 w-full  items-center justify-start flex-row text-[#737373] font-semibold gap-4">
+                      <div className="w-fit">
+              <Image
+                src="/learn/module.svg"
+                alt="Search Icon"
+                width={20}
+                height={20}
+                priority
+                className="w-6 h-6 md:w-6 md:h-6"
+              />
+            </div>
+                      <div className="w-fit    text-center rounded-sm text-sm lg:text-xl">Module:</div>
+                       <div className="w-fit  rounded-sm text-center text-sm lg:text-xl">Data science</div>
+                        </div>
+                      <div className="flex absolute md:top-40   w-full items-center justify-start flex-row gap-3 text-[#737373] font-semibold">
+                      <div className="w-fit">
+              <Image
+                src="/learn/chapter.svg"
+                alt="Search Icon"
+                width={20}
+                height={20}
+                priority
+                className="w-6 h-6 md:w-6 md:h-6"
+              />
+            </div>
+                      <div className="w-fit  text-center rounded-sm text-sm lg:text-xl">Chapter:</div>
+                       <div className="w-fit rounded-sm text-center text-sm lg:text-xl">Supervise learning and applications</div>
                         </div>
                      
-                      <div className="flex absolute md:bottom-14 items-center justify-center">
-                      <div className="flex items-center justify-center">
+                      <div className="flex absolute lg:top-56 md:top-32  items-center w-full justify-center">
+                      <div className="flex items-center justify-end">
                       <Image
                       src="/percentage bar.svg"
                        alt="Verceldcjncn Logo"
                        className="mt-6" // Removed mx-auto, as the parent container is already centered
-                       width={500}
+                       width={400}
                         height={1}
                        priority
                        />
                           </div>
+                        </div>
+
+                        <div className="flex absolute top-72 w-full h-fit text-[15px] font-semibold gap-10 text-center">
+                          <div className="w-fit px-6 py-2 h-fit hover:bg-[#8C52FF] hover:text-white  border-2 border-[#8C52FF] rounded-sm ">View Insights</div>
+                          <div className="w-fit p-2 px-6 h-fit border-2 border-[#8C52FF] hover:bg-[#8C52FF]  hover:text-white rounded-sm">Resume Learning</div>
                         </div>
 
                     </div>
