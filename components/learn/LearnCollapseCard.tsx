d7fa67d4d08cf7f8b16ed6e9b189ec14179c7862
Anubhav shaurya <97554087+anubhav220@users.noreply.github.com>
2024-06-07 20:07:03 +0530

    updated

--- components/learn/LearnCollapseCard.tsx
+++ components/learn/LearnCollapseCard.tsx
@@ -1,12 +1,14 @@
 "use client"
 import LearnCollapseItem from "./LearnCollapseItem";
-
+import { useModalStore } from "./store02";
 import { useState } from "react";
 import Lessonsdiv from "./Lessonsdiv";
 import Image from "next/image";
 import useBooleanStore from './store';
+import Lessonsdiv01 from "./lessondivdropdown";
+import { Modal, ModalContent, ModalBody } from "@nextui-org/react";
 export default function LearnCollapseCard() {
-
+    const { isOpen, closeModal } = useModalStore();
 
     const booleanValue = useBooleanStore((state) => state.booleanValue);
 
@@ -15,7 +17,7 @@ export default function LearnCollapseCard() {
     return (
 
         <>
-            <div className="p-4  text-white flex flex-col  ">
+            <div className="p-4  text-white flex flex-col   ">
                 <div className="w-full h-full flex flex-row gap-2  justify-between mx-6 ">
                     <div className=" h-full  w-[35%]">
                         <LearnCollapseItem />
@@ -27,57 +29,64 @@ export default function LearnCollapseCard() {
                     </div>
                     {
                         booleanValue && <Lessonsdiv />
+
                     }
+                    {
+                        booleanValue && <Lessonsdiv01 />
+
+                    }
+                   
+
                 </div>
-                <div className="w-full h-fit pr-3 pl-6">                
+                <div className="w-full h-fit pr-3 pl-6 lg:block hidden">
                     <div className=" w-full h-36 rounded-lg bg-[#E8E2F4] mb-5 max-w-[1300px] flex flex-row   ">
 
-                    <div className="bg-[#8C52FF] w-40 max-w-44 h-full rounded-lg flex justify-center items-center">
-                        <Image
-                            src="/project.svg"
-                            alt="Verceldcjncn Logo"
-                            width={90}
-                            height={90}
-                            priority={true}
-                            className="h-fit w-fit"
-                        />
+                        <div className="bg-[#8C52FF] w-40 max-w-44 h-full rounded-lg flex justify-center items-center">
+                            <Image
+                                src="/project.svg"
+                                alt="Verceldcjncn Logo"
+                                width={90}
+                                height={90}
+                                priority={true}
+                                className="h-fit w-fit"
+                            />
+
+                        </div>
+
+                        <div className="bg-[#E8E2F4]  w-full justify-around flex-col rounded-lg   ">
+                            <div className=" w-full h-[46%] flex justify-between  border-b-[0.5px] border-[#737373]">
+                                <div className=" w-fit h-fit mt-3 ml-4 "><span className="text-centre text-black font-semibold text-[24px] ... ">Capstone Project</span></div>
+                                <div className=" w-fit h-fit mt-5 mr-7 "><span className="text-centre text-[#8C52FF] font-semibold text-[15px] ... ">View Guidelines</span></div>
+                            </div>
+                            <div className="sm:w-full sm:h-[55%] flex justify-between  sm:ml-4">
+                                <div className=" w-full max-w-[900px] h-fit rounded-lg">
+                                    <p className="text-centre text-[#737373] italic text-[17px] underline  ...">Avengers and Silicon Valley</p>
+                                    <p className="text-centre text-[#737373] text-[14px] mt-2 leading-4 " >
+                                        Avengers: Harness Python to conquer challenges in this Silicon Valley-inspired project. Learn Python and machine learning as you emerge as a tech hero!
+                                    </p>
+
+
+                                </div>
+                                <div className=" w-fit h-full mx-9" >
+                                    <Image
+                                        src="/learn/arrow.svg"
+                                        alt="Verceldcjncn Logo"
+                                        className="w-full h-fit my-6 "
+                                        width={15}
+                                        height={15}
+                                        priority
+
+
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
 
                     </div>
-
-                    <div className="bg-[#E8E2F4]  w-full justify-around flex-col rounded-lg  ">
-                        <div className=" w-full h-[46%] flex justify-between  border-b-[0.5px] border-[#737373]">
-                            <div className=" w-fit h-fit mt-3 ml-4 "><span className="text-centre text-black font-semibold text-[24px] ... ">Capstone Project</span></div>
-                            <div className=" w-fit h-fit mt-5 mr-7 "><span className="text-centre text-[#8C52FF] font-semibold text-[15px] ... ">View Guidelines</span></div>
-                        </div>
-                        <div className="sm:w-full sm:h-[55%] flex justify-between  sm:ml-4">
-                            <div className=" w-full max-w-[900px] h-fit rounded-lg">
-                                <p className="text-centre text-[#737373] italic text-[17px] underline  ...">Avengers and Silicon Valley</p>
-                                <p className="text-centre text-[#737373] text-[14px] mt-2 leading-4 " >
-                                    Avengers: Harness Python to conquer challenges in this Silicon Valley-inspired project. Learn Python and machine learning as you emerge as a tech hero!
-                                </p>
-
-
-                            </div>
-                            <div className=" w-fit h-full mx-9" >
-                                <Image
-                                    src="/learn/arrow.svg"
-                                    alt="Verceldcjncn Logo"
-                                    className="w-full h-fit my-6 "
-                                    width={15}
-                                    height={15}
-                                    priority
-
-
-                                />
-                            </div>
-                        </div>
-                    </div>
-
-
-
-
-
-                </div>
                 </div>
 
             </div>
--- components/learn/lessondivdropdown.tsx
+++ components/learn/lessondivdropdown.tsx
@@ -1,34 +1,34 @@
 "use client";
+
 import React from "react";
 import Image from "next/image";
-import { Modal, ModalContent, ModalHeader, ModalBody } from "@nextui-org/react";
-import { useModalStore } from "./store02";
 
 interface Lesson {
   id: number;
   name: string;
   language: string;
   image1: string;
+  image2: string;
 }
 
 const lessonsData: Lesson[] = [
-  { id: 1, name: "Language of ML", language: "python", image1: "/learn/play button.svg" },
-  { id: 2, name: "Supervised Learning: Classification", language: "javascript", image1: "/learn/icon 1.svg" },
-  { id: 3, name: "Assessment: Python", language: "java", image1: "/learn/icon2.svg" },
-  { id: 4, name: "Hands on: ML", language: "ruby", image1: "/learn/icon 3.svg" },
-  { id: 5, name: "ML: Interact with data", language: "typescript", image1: "/learn/icon 4 copy.svg" }
+  { id: 1, name: "Language of ML", language: "python", image1: "/learn/play button.svg", image2: "/learn/revise.svg" },
+  { id: 2, name: "Supervised Learning: Classification", language: "javascript", image1: "/learn/icon 1.svg", image2: "/learn/completed.svg" },
+  { id: 3, name: "Assessment: Python", language: "java", image1: "/learn/icon2.svg", image2: "/learn/not started.svg" },
+  { id: 4, name: "Hands on: ML", language: "ruby", image1: "/learn/icon 3.svg", image2: "/learn/try again.svg" },
+  { id: 5, name: "ML: Interact with data", language: "typescript", image1: "/learn/icon 4 copy.svg", image2: "/learn/not started.svg" }
 ];
 
-const Lessonsdiv: React.FC = () => (
-  <div className="mx-2 md:mx-6 w-full md:w-[60%] flex justify-center pr-3 ">
-    <div className="bg-white border-[0.5px] border-[#737373] rounded-xl flex flex-col w-full h-fit px-4 md:px-8 pb-4 md:pb-8">
-      <div className="w-full text-black mx-2 my-4 font-medium text-xl">Lessons</div>
-      <div className="h-fit w-full mx-2 md:mx-5 mt-2">
+const Lessonsdiv01: React.FC = () => (
+  <div className="mx-6 w-full md:w-[60%] flex justify-center pr-3 xl:hidden ">
+    <div className="bg-white border-[0.5px] border-[#737373] rounded-xl flex md:flex-col w-full h-fit px-8 pb-8">
+      <div className="w-fit text-black mx-2 my-4 font-[500] text-xl">Lessons</div>
+      <div className="h-fit w-full mx-5 mt-2">
         {lessonsData.map((lesson) => (
-          <div key={lesson.id} className="w-full h-fit overflow-hidden flex flex-col md:flex-row mb-4">
-            <div className="w-full h-14 flex">
-              <div className="w-full md:w-[470px] h-10 flex border border-gray-50 bg-white hover:bg-[#E8E2F4] hover:border-[#E8E2F4] rounded-3xl">
-                <div className="w-fit flex justify-center my-auto items-center mx-2">
+          <div key={lesson.id} className="w-full h-fit overflow-hidden flex flex-row">
+            <div className="w-fit h-14 flex">
+              <div className="w-fit md:w-[470px] h-10 flex border-[1px] border-gray-50 bg-white hover:bg-[#E8E2F4] hover:border-[#E8E2F4] rounded-3xl">
+                <div className="w-fit flex justify-center my-auto items-center mx-[3px]">
                   <Image
                     src={lesson.image1}
                     alt="Lesson Icon"
@@ -40,11 +40,12 @@ const Lessonsdiv: React.FC = () => (
                 </div>
                 <div className="flex justify-center items-center">
                   <h4 className="text-black text-sm mx-2">
-                    <span className="font-semibold">{lesson.name}</span>: {lesson.language}
+                    <span className="font-[660]">{lesson.name}</span>: {lesson.language}
                   </h4>
                 </div>
               </div>
             </div>
+            
           </div>
         ))}
       </div>
@@ -52,21 +53,4 @@ const Lessonsdiv: React.FC = () => (
   </div>
 );
 
-const LessonsModal: React.FC = () => {
-  const { isOpen, closeModal } = useModalStore();
-
-  return (
-    <Modal isOpen={isOpen} onOpenChange={closeModal}>
-      <ModalContent>
-        <>
-          <ModalHeader className="flex flex-col gap-1">Lessons</ModalHeader>
-          <ModalBody>
-            <Lessonsdiv />
-          </ModalBody>
-        </>
-      </ModalContent>
-    </Modal>
-  );
-};
-
-export default LessonsModal;
+export default Lessonsdiv01;
