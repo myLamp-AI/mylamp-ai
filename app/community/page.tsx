49532106795a34797f96b1b7ac8da78d81f9c3bb
kumar1397 <sahuiswar007@outlook.com>
2024-06-09 21:17:03 +0530

    sidebar updated

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,82 +1,105 @@
 "use client"
-import React, { useState, useRef, useEffect } from 'react';
-import { usePathname } from "next/navigation";
 import Image from "next/image";
-import NavLinks from '@/components/navbar/NavItems'
-import navData from '@/components/navbar/navLinks.json'
-import smData from '@/app/data/navsocialicon.json'
-import Community1 from '@/components/community/Community'
+import React, { useState } from 'react';
+import Exdata from '@/app/data/Excommunity.json';
+import Alldata from '@/app/data/Allcommunity.json'
 export default function Community() {
-    // sidebar consts
-    const pathname = usePathname();
-    const navLen = navData.length;
-    const socialLen = smData.length;
-    const navRender = Array(navLen).fill(null);
-    const smRender = Array(socialLen).fill(null);
-    //practice consts
-    //Dummy consts
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const mainContentRef = useRef(null);
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
+    const ExdataLen = Exdata.length;
+    const AlldataLen = Alldata.length;
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % ExdataLen);
     };
-    useEffect(() => {
-        if (mainContentRef.current) {
-            mainContentRef.current.style.width = isSidebarOpen ? 'calc(100% - 200px)' : '100%';
-        }
-    }, [isSidebarOpen]);
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + ExdataLen) % ExdataLen);
+    };
+    const [messageHeading, setMessageHeading] = useState('');
+    const toggleHeading = (text: string) => {
+        setMessageHeading(text);
+    }
 
     return (
-        <div className="flex h-full">
-            {/* Sidebar */}
-            <div className={`${isSidebarOpen ? 'w-56' : 'w-8'} transition-all duration-300 flex flex-row `}>
-                <div className={`${isSidebarOpen ? 'w-48' : 'w-0'} transition-all duration-500`}>
-                    {
-                        isSidebarOpen &&
-                        <div className={`gap-1 flex flex-col bg-[#fff] fixed`}>
-                            <div className="w-fit h-[500px] flex flex-col gap-7 overflow-x-clip overflow-y-auto scrollbar-hide mr-6">
-                                {
-                                    navRender.map((item, navindex) => {
-                                        const isActive = pathname === navData[navindex].Link;
-                                        return (
-                                            <div key={navindex}>
-                                                <NavLinks name={navData[navindex].name} icon={navData[navindex].icon} Link={navData[navindex].Link} index={navindex} />
-                                            </div>
-                                        );
-                                    })
-
-                                }
+        <div className="w-full flex justify-center">
+            <div className="bg-[#F1EAFF] w-full h-100vh max-w-[1200px] flex flex-wrap lg:flex-nowrap gap-3" >
+                <div className="w-2/5 h-[90vh] flex flex-col gap-3 pl-4 pt-3 overflow-auto scrollbar-hide">
+                    <div className=" text-[#737373] font-semibold flex flex-col gap-2.5">
+                        <div className="font-bold">Hello Raj!</div>
+                        <span className="text-[#A6A6A6]">Lear with your peers to maximise learing</span>
+                        <div className="relative">
+                            <input type="text" className="pl-10 pr-4 py-2 w-11/12 border rounded-lg" placeholder="Search Problems" />
+                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                <Image src="/community/search-lens.svg" alt="search" width={25} height={25} />
                             </div>
-                            <div className="w-full flex flex-col items-center">
-                                <div className="w-12 h-12 border-4 rounded-full border-[#f5f5f5] absolute z-10">
-                                    <Image src="/avtar.svg" alt="pfp" height={50} width={50} />
-                                </div>
-                                <div className="w-11/12 h-24 border-2 bg-[#8c52ff] rounded-xl relative top-5"></div>
-                            </div>
-                            <div className="w-full h-20 bg-[#E8E2F4] text-lg font-medium flex flex-col justify-center items-center gap-2 mt-6 rounded-lg">
-                                <div className="flex flex-row gap-1.5 font-semibold">
-                                    {
-                                        smRender.map((item, smindex) => (
-                                            <div key={smindex}>
-                                                <div className="w-7 h-7"><Image src={smData[smindex].icon} alt={smData[smindex].name} height={100} width={100} /></div>
-                                            </div>
-                                        ))
-                                    }
-                                </div>
-                                <span>Connect with us</span>
-                            </div>
-
                         </div>
-                    }
-                </div>
-                <div className='h-[90vh] rounded-lg flex items-center justify-center w-8 relative left-0'><Image src="/sidebar/whiterfarw.svg" height={10} width={10} alt='img' className={`w-8 h-8 bg-[#8c52ff] rounded-full ${isSidebarOpen ? 'rotate-180 delay-300' : ''}`} onClick={toggleSidebar} /></div>
+                    </div>
+                    <div className="flex flex-col gap-3">
+                        <div className="flex flex-row justify-between">
+                            <span className="text-base font-semibold">Exclusive Communities</span>
+                            <div className="flex flex-row gap-4">
+                                <button onClick={handlePrev}><Image src="/community/lfarw.svg" alt="arr" width={7.89} height={13.99} /></button>
+                                <button onClick={handleNext}><Image src="/community/rfarw.svg" alt="arr" width={7.89} height={13.99} /></button>
 
-            </div>
-            {/* Main Content */}
-            <div ref={mainContentRef} className="bg-gray-200 flex-1 transition-all duration-300">
-                <Community1 />
+                            </div>
+                        </div>
+                        <div className="flex transition-transform duration-300 ease-in-out" style={{ transform: `translateX(-${(currentIndex / ExdataLen) * 100}%)` }}>
+                            {Exdata.map((slide, index) => (
+                                <div key={index} className="min-w-[200px] px-3 h-56 ">
+                                    <div className="bg-[#fff] rounded-lg h-full">
+                                        <div><Image src={Exdata[index].svg} alt="img" height={100} width={230} className="w-full" /></div>
+                                        <div className="text-md font-bold h-16 flex flex-row justify-around pt-3">
+                                            <div className="w-3/5">{Exdata[index].name}</div>
+                                            <div className={`w-10 h-10 rounded-full bg-[#8c52ff] text-lg ${Exdata[index].message === 0 ? "hidden" : "flex"} flex justify-center items-center text-[#fff] `}>{Exdata[index].message}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                    </div>
+                    <div className="flex flex-col gap-3 overflow-x-clip">
+                        <div className="flex flex-row justify-between">
+                            <span className="text-base font-semibold">All Communities</span>
+                            <button className="text-sm font-semibold text-[#8c52ff]">See All</button>
+                        </div>
+                        <div className="w-full gap-3 flex flex-col justify-center">
+                            {
+                                Alldata.map((item, index) => (
+                                    <div className="w-full h-20 bg-[#fff] flex flex-row text-md font-bold justify-between items-center rounded-lg" onClick={() => toggleHeading(Alldata[index].name)}>
+                                        <div className="flex flex-row items-center">
+                                            <div className="w-[80px] p-1"><Image src={Alldata[index].svg} alt="img" height={10} width={10} className="w-full" /></div>
+                                            <span className="pl-5">{Alldata[index].name}</span>
+                                        </div>
+                                        <div className={`w-10 h-10 rounded-full bg-[#8c52ff] text-lg ${Alldata[index].message === 0 ? "hidden" : "flex"} flex justify-center items-center text-[#fff] mr-3`}>{Alldata[index].message}</div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col w-3/5 bg-[#fff] rounded-lg m-3 mb-0">
+                    <div className="flex flex-row bg-[#8c52ff] w-full h-16 rounded-lg items-center justify-between">
+                        <div className="flex flex-row gap-14 items-center pl-10">
+                            <div className="w-12 h-12 bg-[#fff] rounded-full flex justify-center items-center"><Image src="/community/webdevchaticon.svg" alt="img" height={10} width={10} className="w-8 h-8" /></div>
+                            <span className="text-xl font-semibold text-[#fff]">{messageHeading}</span>
+                        </div>
+                        <div className="flex flex-row items-center gap-8 pr-6">
+                            <div><Image src="/community/searchdocument.svg" alt="img" height={10} width={10} className="w-8 h-8" /></div>
+                            <div><Image src="/community/arrowdown.svg" alt="img" height={10} width={10} className="w-6 h-6" /></div>
+                        </div>
+                    </div>
+                    <div className="flex flex-grow"></div>
+                    <div className="flex justify-center p-3">
+                        <div className="relative w-[65vw] ">
+                            <input type="text" className="pl-10 pr-4 py-2 w-full border rounded-lg bg-[#D9D9D9]" placeholder="text" />
+                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none justify-between w-full">
+                                <Image src="/community/textplussign.svg" alt="search" width={25} height={25} />
+                                <Image src="/community/sendicon.svg" alt="send" width={25} height={25} className="mr-3" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
-};
-
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,51 @@
+"use client"
 import type { Metadata } from "next";
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Sidebar from '@/components/Sidebar'
 const openSans = Open_Sans({ subsets: ["latin"] });
+import React, { useState, useRef, useEffect } from 'react';
 
-export const metadata: Metadata = {
-  title: "MyLampAi - Home",
-  description: "MyLampAi - Home Page",
-};
+
+// export const metadata: Metadata = {
+//   title: "MyLampAi - Home",
+//   description: "MyLampAi - Home Page",
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   return (
     <html lang="en" className="scroll-smooth focus:scroll-auto">
       <body className={`${openSans.className} bg-[#F1EAFF]`}>
-        {/* <Navbar />          */}
-        {children}  
+        <Navbar />         
+        <div className="flex h-full">
+          {/* Sidebar */}
+          <div className={`${isSidebarOpen ? 'w-48' : 'w-0'} transition-all duration-300 flex flex-row `}>
+            <div className="fixed flex flex-row items-center transition-all duration-300">
+              {
+                isSidebarOpen &&
+                <Sidebar
+                  togglesidebar={toggleSidebar}
+                  isSidebarOpen={isSidebarOpen}
+                  setIsSidebarOpen={setIsSidebarOpen}
+                />
+              }
+              <div className='h-[100vh] rounded-lg w-6 flex items-center' onClick={toggleSidebar}><img src="/sidebar/pparw.svg" className={`w-6 h-6 ${isSidebarOpen ?'rotate-180':''}`}/></div>
+            </div>
+          </div>
+          {/* Main Content */}
+          <div className=" flex-1 transition-all duration-300">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
