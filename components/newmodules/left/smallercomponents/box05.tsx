d355981c012c7681964c3ece0ba07b5afaed8955
Anubhav shaurya <97554087+anubhav220@users.noreply.github.com>
2024-06-12 21:23:47 +0530

    updated

---
+++ components/newmodules/left/smallercomponents/box05.tsx
@@ -0,0 +1,11 @@
+// LeftSide.tsx
+import React from 'react';
+const Box05: React.FC = () => {
+    return (
+        <div className="  h-[105px] w-full  max-w-[350px] bg-[#8C52FF] mx-auto  rounded-lg  ">
+            <div className=" h-[78px] w-full  max-w-[350px] bg-white mx-auto  rounded-lg "></div>
+        </div>
+    );
+};
+
+export default Box05;
--- components/newmodules/parentmid.tsx
+++ components/newmodules/parentmid.tsx
@@ -13,6 +13,7 @@ export default function ParentMid() {
        isOpen && <LeftSide />
       }
       <Middle />
+      {/* <RightSide/> */}
      
 
     </div>
