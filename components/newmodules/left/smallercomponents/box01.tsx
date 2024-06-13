e5976189b87276d30f1d4c7a50d8a7774fac2b39
Anubhav shaurya <97554087+anubhav220@users.noreply.github.com>
2024-06-13 06:14:16 +0530

    module

--- components/newmodules/left/smallercomponents/box01.tsx
+++ components/newmodules/left/smallercomponents/box01.tsx
@@ -1,8 +1,19 @@
 // LeftSide.tsx
 import React from 'react';
+import Image from 'next/image';
 const Box01: React.FC = () => {
     return (
-        <div className="  h-24 w-full max-w-[350px] bg-[#8C52FF] mx-auto  rounded-lg  ">
+        <div className="  h-24 w-full max-w-[350px] bg-[#8C52FF] relative mx-auto  rounded-lg  ">
+             <div className="rounded-full bg-white absolute top-4 left-2 h-16 w-16 flex items-center justify-center">
+              <Image
+              src="/modules/ai.svg"
+              alt="Verceldcjncn Logo"
+              className=""
+              width={42}
+              height={42}
+            />
+            </div>
+            <h1 className="text-left absolute left-28  text-white font-bold text-decoration-line: underline font-size: 0.75rem ...">module 03</h1>
         </div>
     );
 };
