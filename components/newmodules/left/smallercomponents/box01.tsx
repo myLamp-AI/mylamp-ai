72a6ad3e09b12afcf640c3cebb9ed5e3e7ce1b66
Anubhav shaurya <97554087+anubhav220@users.noreply.github.com>
2024-06-13 23:27:01 +0530

    updated

--- components/newmodules/left/smallercomponents/box01.tsx
+++ components/newmodules/left/smallercomponents/box01.tsx
@@ -13,7 +13,13 @@ const Box01: React.FC = () => {
               height={42}
             />
             </div>
-            <h1 className="text-left absolute left-28  text-white font-bold text-decoration-line: underline font-size: 0.75rem ...">module 03</h1>
+             <div className='w-fit h-fit'>
+            <div className=' absolute top-3 left-[83px]'><h1 className="text-left text-white font-bold text-decoration-line: underline font-size: 0.75rem ...">Module 03</h1></div>  
+            <div className=' absolute top-9 left-[83px]'><h1 className=" text-left text-white text-md font-bold">Tech 101: Starting in Tech</h1></div>
+            <div className=' absolute top-14 left-[83px]'><span className=" text-left font-semibold text-white text-[10px]"> 7 lesson   7 lesson    7 lesson </span></div>
+            </div>
+            <div className=" bg-white rounded-full h-4 w-4 absolute right-3  top-10"><Image  src="/modules/arrow.svg" alt="Verceldcjncn Logo" className=""width={15} height={15}/></div>
+          
         </div>
     );
 };
