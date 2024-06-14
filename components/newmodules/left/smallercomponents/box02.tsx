0799e63806d5718573e58a2aaaa2b90ec06ade16
Anubhav shaurya <97554087+anubhav220@users.noreply.github.com>
2024-06-14 11:51:55 +0530

    moudule

--- components/newmodules/left/smallercomponents/box02.tsx
+++ components/newmodules/left/smallercomponents/box02.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import Image from 'next/image';
 const Box02: React.FC = () => {
     return (
-        <div className=" relative h-[100px] w-full my-2 max-w-[350px] bg-white mx-auto  rounded-lg  ">
-             <div className=" rounded-full bg-white absolute top-4 left-2 h-16 w-16 flex items-center justify-center">
+        <div className=" relative h-[100px] w-full my-2 max-w-[350px] bg-white mx-auto  rounded-lg flex justify-between  items-center ">
+             <div className=" mx-2 rounded-full bg-white  h-16 w-16 flex items-center justify-center">
               <Image
               src="/modules/robot (1).svg"
               alt="Verceldcjncn Logo"
@@ -13,7 +13,11 @@ const Box02: React.FC = () => {
               height={90}
             />
             </div>
-            <div className=" rounded-full bg-white absolute top-4 right-2 h-16 w-16 flex items-center justify-center">
+            <div className=' w-fit h-fit flex flex-col items-center'>
+            <h1 className="  font-semibold text-lg ">Grade 47%</h1>
+            <h2 className="  font-semibold text-lg">Credits 4/12</h2>
+            </div>
+            <div className=" mr-2 rounded-full bg-white  h-16 w-16 flex items-center justify-center">
               <Image
               src="/modules/calendar.svg"
               alt="Verceldcjncn Logo"
--- components/newmodules/left/smallercomponents/box03.tsx
+++ components/newmodules/left/smallercomponents/box03.tsx
@@ -1,9 +1,28 @@
 // LeftSide.tsx
 import React from 'react';
+import Image from 'next/image';
+import { useState } from 'react';
 const Box03: React.FC = () => {
+
+    const [progress, setProgress] = useState(90);
+
     return (
         <div className=" mt-16  h-[105px] w-full  max-w-[350px] bg-[#8C52FF] mx-auto  rounded-lg  ">
-            <div className=" mt-16  h-[78px] w-full  max-w-[350px] bg-white mx-auto  rounded-lg "></div>
+            <div className=" mt-16 h-[78px] w-full  max-w-[350px] bg-white mx-auto flex flex-row justify-between items-center  rounded-lg ">
+                <div className="  mx-4  rounded-full bg-white  h-14 w-14  flex items-center justify-center">
+                    <Image src="/modules/ml.svg" alt="Verceldcjncn Logo" className="" width={70} height={70} />
+                </div>
+                <div className='w-full my-2'>
+                    <h1 className="text-left   text-decoration-line: underline 2xl:text-[1.5rem]  font-size: 0.75rem ...">chapter 1</h1>
+                    <h2 className=" text-left 2xl:text-[1.3rem] font-semibold -mt-1">Getting Started with ML</h2>
+                    <div className=" w-full max-w-[265px] my-2   h-[7px] border-[1px] border-[#8C52FF] rounded-lg overflow-hidden">
+                        <div className="bg-[#8C52FF] h-full text-white text-center leading-8" style={{ width: `${progress}%` }}></div>
+                    </div>
+                </div>
+                
+            </div>
+
+
         </div>
     );
 };
