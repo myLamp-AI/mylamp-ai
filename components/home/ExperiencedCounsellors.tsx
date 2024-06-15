f9da4fc7b4fc3d99a7562cf173a74b8695e9e6a8
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2024-06-15 09:24:13 +0530

    .

---
+++ components/home/ExperiencedCounsellors.tsx
@@ -0,0 +1,29 @@
+
+
+const ExperiencedCounsellors = () => {
+    return (
+        <>
+            <div id="experiencedcounsellors" className="pb-[100px] px-[100px] min-h-screen">
+                <h4 className="pt-4 font-semibold text-primary">
+                    EXPERIENCED COUNSELLORS
+                    <div className="bg-primary w-6 h-6 blur-sm rounded-full absolute left-0 translate-x-[-14px] translate-y-[-100%] " ></div>
+                </h4>
+                <div className="text-3xl font-medium mt-8 mb-4">
+                    15+ years of expertise, within your reach
+                </div>
+                <p className="text-[#000000BB] font-medium my-4">
+                    For every phase of your journey, we&apos;ll get you a
+                    specialised, dedicated counsellor! Check out the counsellors &
+                    their records yourselves.
+                </p>
+                <div className="w-full my-8 rounded-2xl">
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ExperiencedCounsellors
--- components/home/WhyWize.tsx
+++ components/home/WhyWize.tsx
@@ -1,4 +1,5 @@
 "use client";
+import ExperiencedCounsellors from "./ExperiencedCounsellors";
 // import Image from "next/image"
 import WizeCampLink from "./WizeCampLink";
 import { useState } from "react";
@@ -75,6 +76,7 @@ export default function WhyWize() {
               </p>
               <div className="bg-white min-h-[400px] w-full my-8 rounded-2xl"></div>
             </div>
+
             <div id="practice" className="pb-[100px] px-[100px] min-h-screen">
               <h4 className="pt-4 font-semibold text-[#8C52FF]">
                 PRACTICE
@@ -89,6 +91,7 @@ export default function WhyWize() {
               </p>
               <div className="bg-white min-h-[400px] w-full my-8 rounded-2xl"></div>
             </div>
+
             <div id="smartestplatform" className="pb-[100px] px-[100px] min-h-screen">
               <h4 className="pt-4 font-semibold text-[#8C52FF]">
                 SMARTEST PLATFORM
@@ -104,21 +107,9 @@ export default function WhyWize() {
               </p>
               <div className="bg-white min-h-[400px] w-full my-8 rounded-2xl"></div>
             </div>
-            <div id="experiencedcounsellors" className="pb-[100px] px-[100px] min-h-screen">
-              <h4 className="pt-4 font-semibold text-[#8C52FF]">
-                EXPERIENCED COUNSELLORS
-                <div className="bg-[#8C52FF] w-6 h-6 blur-sm rounded-full absolute left-0 translate-x-[-14px] translate-y-[-100%] " ></div>
-              </h4>
-              <div className="text-3xl font-medium mt-8 mb-4">
-                15+ years of expertise, within your reach
-              </div>
-              <p className="text-[#000000BB] font-medium my-4">
-                For every phase of your journey, we&apos;ll get you a
-                specialised, dedicated counsellor! Check out the counsellors &
-                their records yourselves.
-              </p>
-              <div className="bg-white min-h-[400px] w-full my-8 rounded-2xl"></div>
-            </div>
+
+            <ExperiencedCounsellors />
+
             <div id="ourwinningrecord" className=" px-[100px] min-h-screen">
               <h4 className="pt-4 font-semibold text-[#8C52FF]">
                 OUR WINNING RECORD
