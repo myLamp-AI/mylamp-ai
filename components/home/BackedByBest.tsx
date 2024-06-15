0dfd5f0e289484e84dd029d87204dc000c62e3ca
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2024-06-15 12:11:23 +0530

    Minor changes

--- components/home/BackedByBest.tsx
+++ components/home/BackedByBest.tsx
@@ -10,23 +10,23 @@ export default function BackedByBest() {
                     <div className="h-1 bg-black w-full bg-gradient-to-r from-primary to-white"></div>
                 </div>
                 <div className='flex flex-wrap w-full items-center justify-evenly'>
-                    <div className='w-1/3 min-h-[300px] flex flex-col justify-between drop-shadow-md font-semibold text-xl items-center '>
+                    <div className='w-1/3 min-h-[280px] flex flex-col justify-between drop-shadow-md font-bold text-xl items-center '>
                         <div className='w-[250px] flex items-center h-[200px]'>
                             <Image src="/home/IIMBanglore.svg" alt="IIT Kharagpur" className='w-full ' width={200} height={200} />
                         </div>
-                        <div>IIM Bangalore</div>
+                        <div className='text-[1.4rem]'>IIM Bangalore</div>
                     </div>
-                    <div className='w-1/3 min-h-[300px] flex flex-col justify-between font-semibold text-xl items-center '>
-                        <div className='w-[350px] flex items-center h-[200px]'>
+                    <div className='w-1/3 min-h-[280px] flex flex-col justify-between font-bold text-xl items-center '>
+                        <div className='w-[440px] flex items-center h-[200px]'>
                             <Image src="/home/microsoft1.svg" alt="IIT Kharagpur" className='w-full' width={200} height={200} />
                         </div>
-                        <div className='drop-shadow-md'>Microsoft</div>
+                        <div className='drop-shadow-md text-[1.4rem]'>Microsoft</div>
                     </div>
-                    <div className='w-1/3 min-h-[300px] flex flex-col justify-between drop-shadow-md font-semibold text-xl items-center '>
+                    <div className='w-1/3 min-h-[280px] flex flex-col justify-between drop-shadow-md font-bold text-xl items-center '>
                         <div className='w-[150px] flex items-center h-[200px]'>
                             <Image src="/home/IITKGP.svg" alt="IIT Kharagpur" className='w-full ' width={200} height={200} />
                         </div>
-                        <div>IIT Kharagpur</div>
+                        <div className='text-[1.4rem]'>IIT Kharagpur</div>
                     </div>
                 </div>
             </div>
