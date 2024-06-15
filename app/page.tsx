0238e5bb3334f285fa94accc4b85134483daca3c
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2024-06-15 07:06:35 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import HeroSection from "@/components/home/HeroSection";
-import AboutWize from '@/components/home/AboutWize';
-import WizeCamp from '@/components/home/WizeCamp';
-import WhyWize from '@/components/home/WhyWize';
-import BackedByBest from '@/components/home/BackedByBest';
-import PowerOfWize from '@/components/home/PowerOfWize';
-import RecentAdvances from '@/components/home/RecentAdvances';
-import CommunitySection from '@/components/home/Community';
-import Footer from '@/components/home/Footer';
-import HomeSlider from '@/components/home/HomeSlider';
+import AboutWize from "@/components/home/AboutWize";
+import WizeCamp from "@/components/home/WizeCamp";
+import WhyWize from "@/components/home/WhyWize";
+import BackedByBest from "@/components/home/BackedByBest";
+import PowerOfWize from "@/components/home/PowerOfWize";
+import RecentAdvances from "@/components/home/RecentAdvances";
+import CommunitySection from "@/components/home/Community";
+import Footer from "@/components/home/Footer";
+import HomeSlider from "@/components/home/HomeSlider";
 import HomeNavbar from "@/components/home/HomeNavbar";
 
 export default function Page() {
   return (
     <>
-      <main className='h-full bg-[#fff]'>
+      <main className="h-full bg-[#fff]">
         <HomeNavbar />
         <HeroSection />
         <AboutWize />
@@ -25,7 +25,7 @@ export default function Page() {
         <RecentAdvances />
         <CommunitySection />
         <Footer />
-      </main> 
+      </main>
     </>
-  )
+  );
 }
