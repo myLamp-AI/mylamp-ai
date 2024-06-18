f83fbcc234aa9e8c2b16114e957332dde99d443b
kumar1397 <sahuiswar007@outlook.com>
2024-06-18 21:49:45 +0530

    carousel changes

--- components/community/NewCarousel.tsx
+++ components/community/NewCarousel.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useRef } from 'react';
 import Image from "next/image";
-import Exdata from '@/app/data/Assessment.json'
+import Exdata from '@/app/data/Excommunity.json'
 
 
 const Carousel: React.FC = () => {
@@ -26,6 +26,9 @@ const Carousel: React.FC = () => {
     }
   };
 
+
+
+
   return (
     <>
       <div className="flex flex-col gap-3 relative">
@@ -37,10 +40,10 @@ const Carousel: React.FC = () => {
           </div>
         </div>
         <div className="carousel-container absolute top-7">
-          <div className="carousel flex space-x-2 overflow-x-auto scrollbar-hide w-[900px]" ref={carouselRef}>
+          <div className="carousel flex space-x-4 overflow-x-auto scrollbar-hide w-[400px]" ref={carouselRef}>
               {Exdata.map((slide, index) => (
                 <div key={index} >
-                  <div className="bg-[#fff] rounded-lg h-full w-56">
+                  <div className="bg-[#fff] rounded-lg h-full w-60">
                     <div ><Image src={Exdata[index].svg} className="w-full" alt="item" height={100} width={100} /></div>
                     <div className="text-md font-bold h-16 flex flex-row justify-around pt-3">
                       <div className="w-3/5">{Exdata[index].name}</div>
