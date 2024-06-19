7fc6b6550fde9f995b16787b76148dac6cf48e76
Anubhav shaurya <97554087+anubhav220@users.noreply.github.com>
2024-06-19 13:35:58 +0530

    modules updated

--- components/newmodules/parentmid.tsx
+++ components/newmodules/parentmid.tsx
@@ -5,7 +5,7 @@ import RightSide from './right/rightside';
 import Middle from './middlecomponents/middlecomponents';
 import useStoreright from './right/zustandright/storeright';
 import useStore from './left/zustandleft/storeleft';
-
+import Sidebar from '../Sidebar';
 export default function ParentMid() {
   const { isOpen } = useStore();
   const { isOpenright } = useStoreright();
