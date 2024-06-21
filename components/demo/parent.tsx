1d87966b95e205e41cd7ac9a5f6a6dbe96a40060
Anubhav shaurya <97554087+anubhav220@users.noreply.github.com>
2024-06-21 20:26:41 +0530

    udpaed

---
+++ components/demo/parent.tsx
@@ -0,0 +1,18 @@
+"use client"
+import Sidebar from "../Sidebar copy";
+import Middle from "./middle";
+import LeftSide from "../newmodules/left/leftside";
+export default function ParentMid() {
+
+  return (
+    <div className="flex h-[91vh] ">
+     
+      
+   <Middle/>
+   <LeftSide/>
+
+    </div>
+  )
+};
+
+
--- components/newmodules/parentmid.tsx
+++ components/newmodules/parentmid.tsx
@@ -1,16 +1,30 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LeftSide from './left/leftside';
 import RightSide from './right/rightside';
 import Middle from './middlecomponents/middlecomponents';
 import useStoreright from './right/zustandright/storeright';
 import useStore from './left/zustandleft/storeleft';
 import Sidebar from '../Sidebar';
+
 export default function ParentMid() {
   const { isOpen } = useStore();
   const { isOpenright } = useStoreright();
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  const checkScreenSize = () => {
+      setIsSmallScreen(window.innerWidth < 768); // Tailwind's sm breakpoint is 640px
+  };
+
+  useEffect(() => {
+      checkScreenSize();
+      window.addEventListener('resize', checkScreenSize);
+
+      return () => window.removeEventListener('resize', checkScreenSize);
+  }, []);
   return (
     <div className="flex h-[91vh] ">
+      
       {
        isOpen && <LeftSide />
       }
