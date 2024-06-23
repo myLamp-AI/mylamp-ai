865233ac6c779025e6d8a165d415b470c1f5d79e
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2024-06-23 20:50:28 +0530

    Minor changes

--- app/Practice/page.tsx
+++ app/Practice/page.tsx
@@ -13,7 +13,7 @@ const Practice = () => {
         <div className="bg-[#F1EAFF] p-5 min-h-screen flex flex-col lg:flex-row justify-between z-0">
             <div className="w-full lg:w-[60%] mb-5 lg:mb-0 ">
                 <div className="mb-5">
-                    <p className="text-[#737373] font-bold text-xl">Let's Rock!</p>
+                    <p className="text-[#737373] font-bold text-xl">Let&apos; Rock!</p>
                     <p className="text-[#A6A6A6] text-sm">Practice regularly to achieve perfection</p>
                 </div>
                 <div className="flex items-center gap-5 mt-5 bg-white rounded-lg px-5 py-2 shadow-md shadow-slate-200">
--- components/home/HomeNavbar.tsx
+++ components/home/HomeNavbar.tsx
@@ -25,7 +25,7 @@ const HomeNavbar = () => {
     return (
         <>
             <div className={`flex justify-between gap-4 ${!scroll ? "bg-primary-foreground" : "bg-[#ffffff20]"} backdrop-blur-sm transition px-8 py-2 sticky top-0 w-full z-50`}>
-                <Image src={"/home/logo.svg"} height={100} width={100} alt='logo' className='w-[130px] drop-shadow-md '></Image>
+                <Link href={"/"}><Image src={"/home/logo.svg"} height={100} width={100} alt='logo' className='w-[130px] drop-shadow-md '></Image></Link>
                 <div className='flex justify-between bg-[#ffffff90] items-center w-full max-w-[600px] gap-2 pr-2 pl-4 py-2 backdrop-blur-md font-medium rounded-full shadow-sm'>
                     <Link href={"/"} className='hover:bg-primary-foreground transition-all py-2 px-4 rounded-full'>Home</Link>
                     <Link href={"/"} className='hover:bg-primary-foreground transition-all py-2 px-4 rounded-full'>Community</Link>
