a95d5c7fafa4339e6f8ba901ef88ecd3807a8a91
Anubhav shaurya <97554087+anubhav220@users.noreply.github.com>
2024-07-06 14:34:53 +0530

    updated

--- components/home/PowerOfWize.tsx
+++ components/home/PowerOfWize.tsx
@@ -6,22 +6,22 @@ export default function PowerOfWize() {
         <>
             <div className='flex justify-between w-full max-w-[1300px] m-auto gap-4 sm:flex-row flex-col  '>
                 <div className='w-full flex flex-col sm:items-start justify-evenly pl-2 items-center my-10 sm:my-0  '>
-                    <p className="w-fit sm:text-sm font-bold text-xl">
+                    <p className="w-fit sm:text-sm font-bold text-xl -ml-96">
                         ----- The Power of wiZe
                     </p>
-                    <div className='flex flex-col items-start max-w-[500px] gap-8 pr-8'>
-                        <p className="sm:text-4xl text-5xl font-medium my-10 sm:my-0 ">
+                    <div className='flex flex-col items-start sm:max-w-[500px] w-[60%] gap-8 pr-8 '>
+                        <p className="sm:text-4xl text-5xl font-medium my-10 sm:my-0  ">
                             Your Gateway To Limitless Creativity - <span className='text-[#5FE8F1]'>One</span> platform, <span className='text-[#8C52FF]'>Millions</span> of possibilities
                         </p>
                         <button className='flex items-center  gap-2 text-xl border-2 border-black  rounded-full px-4 py-2 font-semibold hover:scale-105 transition-all ease-in-out'>
                             Learn More <Image src={"/home/ArrowLearnMore.svg"} alt='' height={30} width={30} /> 
                         </button>
                     </div>
-                    <p className="sm:text-sm font-bold w-fit mt-9 text-lg ">
+                    <p className="sm:text-sm font-bold w-fit mt-9 text-lg -ml-48 sm:ml-0">
                         Trusted by leading professionals and visionary partners ----
                     </p>
                 </div>
-                <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 w-full max-w-[600px]  mx-auto sm:relative">
+                <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 sm:w-full sm:max-w-[600px] w-[60%]  mx-auto sm:relative">
                     <Image width={100} height={100} src="/home/PowerOfWize.svg" alt="Background Image" className="w-[298px] rounded-tr-[5rem] rounded-bl-[5rem] h-full" />
                     <Image width={100} height={100} src="/home/PowerOfWize.svg" alt="Background Image" className="w-[298px] rounded-br-[5rem] rounded-tl-[5rem]  h-full" />
                     <Image width={100} height={100} src="/home/PowerOfWize.svg" alt="Background Image" className="w-[298px] rounded-tl-[5rem] rounded-br-[5rem]  h-full" />
