f657f0193c3f1f6bb4b1f02a834959becc6ec061
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2024-07-11 22:48:04 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'avatar.vercel.sh',
-            port: '',
-            pathname: '/**',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatar.vercel.sh',
+        port: '',
+        pathname: '/**',
       },
+    ],
+  },
+
+  output: "standalone"
 };
 
 export default nextConfig;
